88e31b9242455f1fcf362c155d4870e3f21f7afa
Naomi Doukhan <naomi.doukhan3@gmail.com>
2023-12-12 12:48:09 +0100

    Controller, Route, et View du show créés

--- app/controllers/homebases_controller.rb
+++ app/controllers/homebases_controller.rb
@@ -1,2 +1,8 @@
 class HomebasesController < ApplicationController
+
+  def show
+    @homebase = Homebase.find_by(user_id: current_user.id)
+
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.destroy_all
+Homebase.destroy_all
+
+User.new(username: "Naomi", email: "naomi@exemple.com", nickname: "naomi", level: 3, experience: 15, energy: 35,  password: "pipicaca").save!
+Homebase.new(user_id: User.last.id, name: "Homebase Testing", coordinates: "1,1", wood_quantity: 100, stone_quantity: 100, gold_quantity: 100, food_quantity: 100, research_ongoing: false, construction_ongoing: false, defense_ongoing: false).save!
