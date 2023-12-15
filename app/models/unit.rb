f6eaa64248ab9c566ea7cd7ad402caae2123a7c1
Steven Despaux <stevendespaux@wanadoo.fr>
2023-12-15 15:55:37 +0100

    the idea is there, only need to feed it with stats now

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -2,11 +2,10 @@ class Unit < ApplicationRecord
   attr_accessor :qty
   belongs_to :town
   validates :name, :level, :hp, :armor_type, :attack, :attack_type, :speed, :stealth, :gold_recruit_cost, :food_recruit_cost, :energy_recruit_cost, :gold_train_cost, :food_train_cost, :energy_train_cost, presence: true
-  validates :enrolled, inclusion: [true, false]
 
   validates :armor_type, inclusion: { in: %w(light medium heavy),
     message: "%{value} is not a valid armor type" }
-  validates :attack_type, inclusion: { in: %w(physical magic),
+  validates :attack_type, inclusion: { in: %w(physical magical),
     message: "%{value} is not a valid attack type" }
 
   after_create :set_role
