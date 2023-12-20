8a0d92ddadf87089668834fa35bad1284483bb26
Steven Despaux <138658660+Rigueur@users.noreply.github.com>
2023-12-20 08:33:36 +0100

    Update battles_controller.rb

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -19,7 +19,7 @@ class BattlesController < ApplicationController
       redirect_to request.referrer
       # check if defending_town is under protection
     elsif @battle.defending_town.last_attacked_at && Time.current - @battle.defending_town.last_attacked_at < 5.hours
-      flash[:alert] = "This town cannot be attacked."
+      flash[:alert] = "This town is already under attack."
       redirect_to request.referrer
     else
       # Update defending_town's resources before calculating the result
