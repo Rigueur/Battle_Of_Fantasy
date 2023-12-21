ed5d3628b0c16818b305e0520450fb294d364e19
Pierre Karst <pierre.karst@edu.heip.fr>
2023-12-21 15:08:44 +0100

    done meeeeeeeeeeeeeeeeeec coucou je suis teub man

---
+++ app/javascript/controllers/battleswiper_controller.js
@@ -0,0 +1,31 @@
+import { Controller } from "@hotwired/stimulus"
+import Swiper from "swiper/bundle"
+
+// Connects to data-controller="swiper"
+export default class extends Controller {
+  connect() {
+    var swiper = new Swiper(".swiper", {
+      grabCursor: true,
+      mousewheel: {
+        invert: false,
+      },
+      scrollbar: {
+        el: ".swiper-scrollbar",
+        draggable: true,
+      },
+      slidesPerView: 1,
+      spaceBetween: 10,
+      // Responsive breakpoints
+      breakpoints: {
+        900: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+      },
+    });
+  }
+}
